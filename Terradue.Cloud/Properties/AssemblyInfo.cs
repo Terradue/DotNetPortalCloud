43503caf0c3b86f7353d769793e2b2e303f823e1
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2016-06-09 11:55:06 +0200

    Added license
    Signed-off-by: Emmanuel Mathot <emmanuel.mathot@gmail.com>

--- Terradue.Cloud/Properties/AssemblyInfo.cs
+++ Terradue.Cloud/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.CompilerServices;
 
 \namespace Terradue.Cloud
 @{
-    Terradue.Cloud Software Package
+    Terradue Cloud module deals with the layer communicating with Cloud Providers interfaces. It provides with an abstract layer to cloud functionalities such as virtual machine provisioning, network infrastructure configuration, disks volumes mountage...
 
-    \xrefitem sw_version "Versions" "Software Package Version" 1.2.14
+    \xrefitem sw_version "Versions" "Software Package Version" 1.2.15
 
     \xrefitem sw_link "Links" "Software Package List" [Terradue.Cloud](https://git.terradue.com/sugar/terradue-cloud)
 
-    \xrefitem sw_license "License" "Software License" [incubating](https://git.terradue.com/sugar/terradue-cloud)
+    \xrefitem sw_license "License" "Software License" [AGPL](https://git.terradue.com/sugar/terradue-cloud/LICENSE)
 
     \xrefitem sw_req "Require" "Software Dependencies" \ref Terradue.Portal
     
