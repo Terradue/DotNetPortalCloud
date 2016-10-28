90e6d5fc0305e4eb5a4299fb2562762e9149037b
eboissier <enguerran.boissier@terradue.com>
2016-10-28 18:04:40 +0200

    up packages

--- Terradue.Cloud/Resources/db/db-1.3#.sql
+++ Terradue.Cloud/Resources/db/db-1.3#.sql
@@ -9,5 +9,30 @@ ALTER TABLE cloud
 ;
 -- RESULT
 
+-- Update priv for cloud ... \
+SET @type_id = (SELECT id FROM type WHERE class='Terradue.Cloud.CloudAppliance, Terradue.Cloud');
+SET @priv_pos = (SELECT MAX(pos) FROM priv);
+DELETE FROM priv WHERE id_type=@type_id AND operation='V';
+DELETE FROM priv WHERE id_type=@type_id AND operation='A';
+INSERT INTO priv (id_type, identifier, operation, pos, name, enable_log) VALUES
+    (@type_id, 'cloud-s', 's', @priv_pos + 1, 'Cloud appliances: search', 1);
+INSERT INTO priv (id_type, identifier, operation, pos, name, enable_log) VALUES
+    (@type_id, 'cloud-c', 'c', @priv_pos + 1, 'Cloud appliances: control', 1);
+UPDATE priv SET identifier='cloud-v' WHERE id_type=@type_id AND operation='v';
+UPDATE priv SET identifier='cloud-m' WHERE id_type=@type_id AND operation='m';
+-- RESULT
+
+-- Update priv for cloudprov ... \
+SET @type_id = (SELECT id FROM type WHERE class='Terradue.Cloud.CloudProvider, Terradue.Cloud');
+SET @priv_pos = (SELECT MAX(pos) FROM priv);
+DELETE FROM priv WHERE id_type=@type_id AND operation='V';
+DELETE FROM priv WHERE id_type=@type_id AND operation='A';
+INSERT INTO priv (id_type, identifier, operation, pos, name, enable_log) VALUES
+    (@type_id, 'cloudprov-s', 's', @priv_pos + 1, 'Cloud providers: search', 1);
+INSERT INTO priv (id_type, identifier, operation, pos, name, enable_log) VALUES
+    (@type_id, 'cloudprov-c', 'c', @priv_pos + 1, 'Cloud providers: control', 1);
+UPDATE priv SET identifier='cloudprov-v' WHERE id_type=@type_id AND operation='v';
+UPDATE priv SET identifier='cloudprov-m' WHERE id_type=@type_id AND operation='m';
+-- RESULT
 
 /*****************************************************************************/
